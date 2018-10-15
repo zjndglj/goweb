a634a15754c378ccd21ca469c2cf49c6642a0b2e
WAD1001 <zjndglj@163.com>
2018-10-15 21:29:52 +0800

    html

--- main.go
+++ main.go
@@ -14,16 +14,26 @@ type myForm struct {
 func main() {
 	pack.Printsl()
 	r := gin.Default()
+	// r.Static("/", "views")
+	r.Static("/css", "views/css")
+	r.Static("/js", "views/js")
+	r.Static("/img", "views/img")
 
-	r.LoadHTMLGlob("views/*")
+	r.LoadHTMLGlob("views/*.html")
 	r.GET("/", indexHandler)
+	r.GET("/index.html", indexHandler)
+	r.GET("/form1.html", form1Handler)
 	r.POST("/", formHandler)
 
 	r.Run(":8080")
 }
 
 func indexHandler(c *gin.Context) {
-	c.HTML(200, "form.html", nil)
+	c.HTML(200, "index.html", nil)
+}
+
+func form1Handler(c *gin.Context) {
+	c.HTML(200, "form1.html", nil)
 }
 
 func formHandler(c *gin.Context) {
