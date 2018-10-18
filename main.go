c1dbdf46d1a3b187725e4d20caaecac52b999576
WAD1001 <zjndglj@163.com>
2018-10-18 21:55:09 +0800

    fix

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"pack"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,15 +15,15 @@ type myForm struct {
 func main() {
 	pack.Printsl()
 	r := gin.Default()
-	// r.Static("/", "views")
-	r.Static("/css", "views/css")
-	r.Static("/js", "views/js")
-	r.Static("/img", "views/img")
+	r.Static("/views/static/", "views/static")
+	// r.Static("/views/static/css/", "views/static/css")
+	// r.Static("/views/static/js/", "/views/static/js")
+	// r.Static("/views/static/img/", "views/static/img")
 
-	r.LoadHTMLGlob("views/*.html")
+	r.LoadHTMLGlob("views/template/*.html")
 	r.GET("/", indexHandler)
-	r.GET("/index.html", indexHandler)
-	r.GET("/form1.html", form1Handler)
+	r.GET("/views/template/*.html", otherHandler)
+	// fmt.Println(abc)
 	r.POST("/", formHandler)
 
 	r.Run(":8080")
@@ -32,8 +33,14 @@ func indexHandler(c *gin.Context) {
 	c.HTML(200, "index.html", nil)
 }
 
-func form1Handler(c *gin.Context) {
-	c.HTML(200, "form1.html", nil)
+func otherHandler(c *gin.Context) {
+	url1 := c.Request.URL.Path
+	fmt.Println(url1)
+	x := strings.Split(url1, "/")
+	// s1 := "views/template/" + x[2] + ".html"
+	s1 := x[3]
+	fmt.Println(s1)
+	c.HTML(200, s1, nil)
 }
 
 func formHandler(c *gin.Context) {
